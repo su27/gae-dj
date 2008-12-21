b075d4c74ea808e6aaff84b9def60b57944db6a7
Dan Su <damn.su@gmail.com>
2008-12-21 23:35:26 +0800

    client

--- static/gae-dj.js
+++ static/gae-dj.js
@@ -8,24 +8,28 @@ Gaedj.addcb = function(cb){
     return Gaedj.cb.length-1
 }
 
-Gaedj.prototype.nload = function(file,callback){
+Gaedj.prototype.nload = function(file){
     var userAgent = navigator.userAgent.toLowerCase();
     var msie = /msie/.test(userAgent) && !/opera/.test(userAgent);
     var e = document.createElement('script');
     e.setAttribute('language','javascript');
     e.setAttribute('type','text/javascript');
     e.setAttribute('src',file);
-    document.getElementsByTagName('head')[0].appendChild(e);
-    if(callback){
-        if(msie){
-            e.onreadystatechange = function(){
-                if(this.readyState==='loaded'||
-                    this.readyState==='complete'){
-                    callback()
-                }
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(e);
+    var cleanup = function(){
+        setTimeout(function(){
+            try{head.removeChild(e)}catch(e){}
+        },100)
+    };
+    if(msie){
+        e.onreadystatechange = function(){
+            if(this.readyState==='loaded'||
+                this.readyState==='complete'){
+                cleanup()
             }
-        } else { e.onload = callback }
-    }
+        }
+    } else { e.onload = cleanup }
 }
 
 Gaedj.prototype.send = function(url,cb){
